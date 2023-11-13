6236006d050de6abd6e0f55ba307e2d3ef89541d
Kevin Lavelle <kevinglavelle@gmail.com>
2023-11-12 21:17:22 -0800

    new login layout

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -6,27 +6,40 @@ import java.awt.event.ActionListener;
 public class LoginApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            // Create a JFrame with a fixed size
             JFrame frame = new JFrame("Login");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(300, 150);
+            frame.setSize(1400, 750); // Set a fixed size (width x height)
+            frame.setResizable(false); // Make the window non-resizable
 
-            JPanel panel = new JPanel();
-            frame.add(panel);
+            JPanel mainPanel = new JPanel();
+            frame.add(mainPanel, BorderLayout.CENTER); // Center the main panel
 
-            panel.setLayout(new GridLayout(3, 2)); // 3 rows, 2 columns
+            // Use a GridLayout for the login components
+            JPanel loginPanel = new JPanel();
+            loginPanel.setLayout(new GridLayout(2, 2)); // 2 rows, 2 columns
+            mainPanel.add(loginPanel, BorderLayout.CENTER); // Place loginPanel in the center
 
             JLabel usernameLabel = new JLabel("Username:");
-            JTextField usernameField = new JTextField();
+            JTextField usernameField = new JTextField(10);
             JLabel passwordLabel = new JLabel("Password:");
-            JPasswordField passwordField = new JPasswordField();
-            JButton loginButton = new JButton("Login");
+            JPasswordField passwordField = new JPasswordField(10);
 
-            panel.add(usernameLabel);
-            panel.add(usernameField);
-            panel.add(passwordLabel);
-            panel.add(passwordField);
-            panel.add(new JLabel()); // Empty label for spacing
-            panel.add(loginButton);
+            loginPanel.add(usernameLabel);
+            loginPanel.add(usernameField);
+            loginPanel.add(passwordLabel);
+            loginPanel.add(passwordField);
+
+            JPanel buttonPanel = new JPanel();
+            mainPanel.add(buttonPanel, BorderLayout.SOUTH); // Place buttonPanel at the bottom
+
+            JButton loginButton = new JButton("Login");
+            JButton createAccountButton = new JButton("Create Account");
+            JButton managerInterfaceButton = new JButton("Manager Interface");
+
+            buttonPanel.add(loginButton);
+            buttonPanel.add(createAccountButton);
+            buttonPanel.add(managerInterfaceButton);
 
             loginButton.addActionListener(new ActionListener() {
                 @Override
@@ -40,6 +53,21 @@ public class LoginApp {
                 }
             });
 
+            createAccountButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Handle create account button click
+                }
+            });
+
+            managerInterfaceButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Handle manager interface button click
+                }
+            });
+
+            frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
     }
